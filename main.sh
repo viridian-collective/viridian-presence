3eb504e3d10a36dfdbac26bc83729defa87042b7
Alex <alexeusgr@gmail.com>
2025-09-09 10:57:53 +0700

    check both nodes

---
+++ main.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Check if a file exists
+if [ -f file.txt ]; then
+  echo "File exists!"
+else
+  echo "File does not exist!"
+fi
+# Check if a program is running:
+
+# Loop over a list of items
+for item in "apple" "banana" "orange"; do
+  echo $item
+done
+
+# Define a function
+greet() {
+  echo "Hello, $1!"
+}
+
+# Call the function
+greet "OSC"
+
+
+# Cardano transaction is a special type of function.
+# Let's call cardano-node 
+echo "cardano-node is running"
+~/workshop/cardano-node/query-tip.sh
+echo "rad node is running"
+rad node status
+# lets use our PPBL for the code sample
+# https://app.andamio.io/course/4a79b279593a787b79da46df4dc34a3e59b003838dcf48a2f436094d/102/lesson/1
+
